2444f4d584d5f8b84ea33d4c533898bdb6a21f5c
Amirulhaziq11 <amirulhq05@gmail.com>
2025-11-24 22:20:07 +0800

    pipeline.py for data processing

--- src/egt309_proj1/pipelines/data_processing/pipeline.py
+++ src/egt309_proj1/pipelines/data_processing/pipeline.py
@@ -1,26 +1,19 @@
 from kedro.pipeline import Node, Pipeline
 
-from .nodes import create_model_input_table, preprocess_companies, preprocess_shuttles
-
+from .nodes import create_model_input_table, preprocess_bmarket
 
 def create_pipeline(**kwargs) -> Pipeline:
     return Pipeline(
         [
             Node(
-                func=preprocess_companies,
-                inputs="companies",
-                outputs="preprocessed_companies",
-                name="preprocess_companies_node",
-            ),
-            Node(
-                func=preprocess_shuttles,
-                inputs="shuttles",
-                outputs="preprocessed_shuttles",
-                name="preprocess_shuttles_node",
+                func=preprocess_bmarket,
+                inputs="bmarket",
+                outputs="preprocessed_bmarket",
+                name="preprocess_bmarket_node",
             ),
             Node(
                 func=create_model_input_table,
-                inputs=["preprocessed_shuttles", "preprocessed_companies", "reviews"],
+                inputs=["preprocessed_bmarket"],
                 outputs="model_input_table",
                 name="create_model_input_table_node",
             ),
