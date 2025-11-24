25d905ec4b4810e4ba283d19e12abbe3d5dbd0b4
Amirulhaziq11 <amirulhq05@gmail.com>
2025-11-24 22:47:31 +0800

    First ML model commit'

--- src/egt309_proj1/pipelines/data_science/nodes.py
+++ src/egt309_proj1/pipelines/data_science/nodes.py
@@ -1,7 +1,7 @@
 import logging
 
 import pandas as pd
-from sklearn.linear_model import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import max_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
 
