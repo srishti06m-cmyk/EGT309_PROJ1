a95ad68a120454194d5dabd8332b1f8112300f3f
Amirulhaziq11 <amirulhq05@gmail.com>
2025-11-28 12:11:25 +0800

    pipeline.py for reporting pipeline

--- src/egt309_proj1/pipelines/reporting/pipeline.py
+++ src/egt309_proj1/pipelines/reporting/pipeline.py
@@ -1,9 +1,11 @@
 from kedro.pipeline import Node, Pipeline
 
 from .nodes import (
-    compare_passenger_capacity_exp,
-    compare_passenger_capacity_go,
+    compare_subscription_by_occupation,
+    compare_subscription_by_contact_method,
     create_confusion_matrix,
+    plot_model_metrics,
+    plot_feature_importance_for_MLModels
 )
 
 
@@ -12,19 +14,29 @@ def create_pipeline(**kwargs) -> Pipeline:
     return Pipeline(
         [
             Node(
-                func=compare_passenger_capacity_exp,
-                inputs="preprocessed_shuttles",
-                outputs="shuttle_passenger_capacity_plot_exp",
+                func=compare_subscription_by_occupation,
+                inputs="load_raw_data",
+                outputs="Comparison_Subscription_by_Occupation_plot",
             ),
             Node(
-                func=compare_passenger_capacity_go,
-                inputs="preprocessed_shuttles",
-                outputs="shuttle_passenger_capacity_plot_go",
+                func=compare_subscription_by_contact_method,
+                inputs="load_raw_data",
+                outputs="Comparison_Subscription_by_Contact_Method_plot",
             ),
             Node(
                 func=create_confusion_matrix,
-                inputs="companies",
-                outputs="dummy_confusion_matrix",
+                inputs="bank_marketing",
+                outputs="Models_Confusion_Matrix",
+            ),
+            Node(
+                func=plot_model_metrics,
+                inputs="results",
+                outputs="Model_Performance_Plots",
+            ),
+            Node(
+                func=plot_feature_importance_for_MLModels,
+                inputs=["rf_model", "gb_model", "lr_model", "feature_names"],
+                outputs="Feature_Importance_Plots_for_MLModels",
             ),
         ]
     )
